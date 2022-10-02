e72891ad4416e4a701f1e8aa49fa47fefd1da698
Vithuran <vithuran.sada@gmail.com>
2022-10-02 02:11:58 -0400

    Working TickTacToe game!

--- src/routes/TicTacToe/+page.svelte
+++ src/routes/TicTacToe/+page.svelte
@@ -1,74 +1,90 @@
 <script>
-    import { afterUpdate } from 'svelte';
+	
+    let tic = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
 
-    import Tile from "../../lib/Tile.svelte";
+	let tac = true;
 
-    let tac = true;
+	let toes = Array(9).fill('');
 
 
-    let toes = [
-        undefined, undefined, undefined,
-        undefined, undefined, undefined,
-        undefined, undefined, undefined
-    ]
-
-    let winMap = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ]
-
-    /**
-	 * @param {any[] | undefined} [toes]
+	/**
+	 * @param {string[]} toes
+	 * @param {number} index
 	 */
-    function checkBoard(toes) {
-        winMap.forEach(winPattern => {
-            if(toes[winPattern[0]] && toes[winPattern[1]] && toes[winPattern[2]]){
-                if(toes[winPattern[0]] === toes[winPattern[1]] && toes[winPattern[1]] === toes[winPattern[2]]){
-                    console.log(winPattern)
-                    return alert(`${toes[winPattern[0]]} Wins!  ${winPattern}` );
-                    
-                }
-            }
-        });
-        
-    }
-
+	async function checkBoard(toes, index) {
+		tic
+			.filter((winPattern) => winPattern.find((i) => i === index))
+			.forEach((winPattern) => {
+				if (
+					toes[winPattern[0]] === toes[winPattern[1]] &&
+					toes[winPattern[1]] === toes[winPattern[2]] &&
+                    toes[winPattern[0]] !== ''
+				) {
+                    console.log(winPattern);
+                    return alert(`${toes[winPattern[0]]} Wins!  ${winPattern}`);
+				}
+			});
+	}
 </script>
 
 <h1>Tic Tac Toe</h1>
 <div class="game">
-    <div class="board">
-        {#each toes as toe, i }
-        <div  on:click={() => {
-            toes[i] = tac ? "X" : "O";
-            tac = !tac;
-            checkBoard(toes)
-        }}>
-            <Tile tic={toe}/>        
-
-        </div>
-        {/each}  
-    </div>
+	<div class="board">
+		{#each toes as toe, i}
+			<div
+				class="tile {toe}"
+				on:click={() => {
+					toes[i] = tac ? 'X' : 'O';
+					tac = !tac;
+					checkBoard(toes, i);
+				}}
+			/>
+		{/each}
+	</div>
 </div>
 
 <style>
-    .game {
+	.game {
+		display: flex;
+		justify-content: center;
+	}
+	.board {
+		display: grid;
+		grid-gap: 1em;
+		grid-template:
+			'tile tile tile'
+			'tile tile tile'
+			'tile tile tile';
+		background-color: brown;
+	}
+
+	.tile {
         display: flex;
         justify-content: center;
-    }
-    .board {
-        display: grid;
-        grid-gap: 1em;
-        grid-template: 
-            'tile tile tile'
-            'tile tile tile'
-            'tile tile tile';
-        background-color: brown;
-    }
+        align-content: center;
+		background-color: bisque;
+		height: 20vw;
+		width: 20vw;
+	}
+	.X::before {
+		content: 'X';
+		font-size: 15vw;
+		font-style: normal;
+		font-family: cursive;
+	}
+	.O::before {
+		content: 'O';
+		font-size: 15vw;
+		font-style: normal;
+		font-family: cursive;
+	}
 </style>
