553501c11901c81e24482cab891dc3b1c45fbcee
Siyaman <siyaman@gmail.com>
2023-01-09 17:40:08 -0800

    Replace Kotlin identifier with JS equivalents
    - private fun -> function
    - val -> const
    - var -> let

--- src/routes/TicTacToe/AIPlayer.ts
+++ src/routes/TicTacToe/AIPlayer.ts
@@ -1,11 +1,4 @@
-package com.siyaman.tictactoe
-
-import kotlin.Double.Companion.NEGATIVE_INFINITY
-import kotlin.Double.Companion.POSITIVE_INFINITY
-import kotlin.math.max
-import kotlin.math.min
-
-private const val WIN_VALUE = 10.0
+const WIN_VALUE = 10.0
 
 /**
  * Calculate score of the Game for a player
@@ -15,7 +8,7 @@ private const val WIN_VALUE = 10.0
  *
  * @return     Score of the current Game {score ∈ ℚ}
  */
-private fun evaluationFunction(game: Game, maximizingPlayer: Player) = when (game.status) {
+function evaluationFunction(game: Game, maximizingPlayer: Player) = when (game.status) {
   is Status.XWon -> if (maximizingPlayer == Player.X) WIN_VALUE else -WIN_VALUE
   is Status.OWon -> if (maximizingPlayer == Player.O) WIN_VALUE else -WIN_VALUE
   else -> 0.0
@@ -39,24 +32,24 @@ private fun evaluationFunction(game: Game, maximizingPlayer: Player) = when (gam
  *
  * @return     Highest score possible for provided Game and depth searched
  */
-private fun alphabeta(
+function alphabeta(
   game: Game, maximizingPlayer: Player, depth: Int,
   alpha: Double = NEGATIVE_INFINITY, beta: Double = POSITIVE_INFINITY
 ): Double {
-  var newAlpha = alpha
-  var newBeta = beta
-  val moves = game.moves
+  let newAlpha = alpha
+  let newBeta = beta
+  const moves = game.moves
   if (depth == 0 || moves.isEmpty()) {
     return evaluationFunction(game, maximizingPlayer)
   }
 
-  val isSamePlayer = game.player == maximizingPlayer
-  var result = if (isSamePlayer) NEGATIVE_INFINITY else POSITIVE_INFINITY
+  const isSamePlayer = game.player == maximizingPlayer
+  let result = if (isSamePlayer) NEGATIVE_INFINITY else POSITIVE_INFINITY
   for (move in moves) {
     if (newAlpha >= newBeta) break
-    val nextGame = game.deepCopy()
+    const nextGame = game.deepCopy()
     nextGame.executeMove(move)
-    val rating = alphabeta(
+    const rating = alphabeta(
       nextGame, maximizingPlayer, depth - 1,
       newAlpha, newBeta
     )
@@ -74,10 +67,10 @@ private fun alphabeta(
 /**
  * Difficulty of artificial intelligence (AI) player
  */
-enum class Difficulty(val depth: Int) {
-  Easy(2),
-  Medium(3),
-  Hard(5)
+enum Difficulty {
+  Easy = 2,
+  Medium = 3,
+  Hard = 5
 }
 
 /**
@@ -89,17 +82,17 @@ enum class Difficulty(val depth: Int) {
  *
  * @return     Best move for selected difficulty
  */
-fun selectMove(game: Game, difficulty: Difficulty = Difficulty.Medium): Int {
-  require(!game.isGameOver) { "Game is over, status ${game.status}" }
-  val moves = game.moves
+export function selectMove(game: Game, difficulty: Difficulty = Difficulty.Medium): number {
+  console.assert(!game.isGameOver, `Game is over, status ${game.status}`)
+  const moves = game.moves
   if (moves.size == 1) return moves.first()
 
-  var maxRating = NEGATIVE_INFINITY
-  var bestMove = moves.first()
+  let maxRating = NEGATIVE_INFINITY
+  let bestMove = moves.first()
   for (move in moves) {
-    val nextGame = game.deepCopy()
+    const nextGame = game.deepCopy()
     nextGame.executeMove(move)
-    val rating =
+    const rating =
       alphabeta(nextGame, game.player, difficulty.depth, maxRating)
     if (maxRating < rating) {
       maxRating = rating
